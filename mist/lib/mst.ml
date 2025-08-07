57bea8a78b45fdd1361fe085e2dd9c63bad0b4c7
futurGH <me@futuristick.ca>
2025-08-07 17:06:02 -0400

    Remove uses of Lwt_main.run, make MST lazy

--- mist/lib/mst.ml
+++ mist/lib/mst.ml
@@ -30,16 +30,24 @@ let encode_node_raw node : Dag_cbor.value =
        [ ("l", match node.l with Some l -> `Link l | None -> `Null)
        ; ("e", `Array (Array.of_list (List.map encode_entry_raw node.e))) ] )
 
-type node_hydrated =
+type node =
   { layer: int
-  ; mutable left: node_hydrated option
-  ; mutable entries: entry_hydrated list }
+  ; mutable left: node option Lwt.t Lazy.t
+  ; mutable entries: entry list }
 
-and entry_hydrated =
-  {layer: int; key: string; value: Cid.t; right: node_hydrated option}
+and entry =
+  {layer: int; key: string; value: Cid.t; right: node option Lwt.t Lazy.t}
+
+let ( let*? ) lazy_opt_lwt f =
+  let%lwt result = Lazy.force lazy_opt_lwt in
+  f result
+
+let ( >>? ) lazy_opt_lwt f =
+  let%lwt result = Lazy.force lazy_opt_lwt in
+  f result
 
 (* figures out where to put an entry in or below a hydrated node, returns new node *)
-let rec insert_entry node entry : node_hydrated Lwt.t =
+let rec insert_entry node entry : node Lwt.t =
   let entry_layer = Util.leading_zeros_on_hash entry.key in
   (* as long as node layer <= entry layer, create a new node above node
      until we have a node at the correct height for the entry to be inserted *)
@@ -47,17 +55,19 @@ let rec insert_entry node entry : node_hydrated Lwt.t =
     if layer >= entry_layer then node
     else
       build_insert_node
-        {layer= layer + 1; left= Some node; entries= []}
+        {layer= layer + 1; left= lazy (Lwt.return_some node); entries= []}
         (layer + 1)
   in
   let insert_node = build_insert_node node node.layer in
   (* if entry is below node, recursively insert into node's left subtree *)
   if entry_layer < insert_node.layer then
-    match (insert_node.entries, insert_node.left) with
+    let*? left = insert_node.left in
+    match (insert_node.entries, left) with
     | [], None ->
         failwith "found totally empty mst node"
     | [], Some left ->
-        node.left <- Some (Lwt_main.run (insert_entry left entry)) ;
+        let%lwt left_inserted = insert_entry left entry in
+        node.left <- lazy (Lwt.return_some left_inserted) ;
         Lwt.return insert_node
     | _ ->
         Lwt.return insert_node
@@ -86,17 +96,18 @@ let find_entry_nonrec node key =
   in
   aux node.entries
 
-(* hydrates a list of entries with their keys; layer and right value are placeholders *)
-let hydrate_entries_keys_only node =
-  node.e
+(* from a list of raw entries, produces a list of their keys *)
+let entries_to_keys entries =
+  entries
   |> List.fold_left
-       (fun (prev_path, entries) entry ->
-         let prefix = String.sub prev_path 0 entry.p in
+       (fun keys entry ->
+         let prefix =
+           match keys with [] -> "" | prev :: _ -> String.sub prev 0 entry.p
+         in
          let path = String.concat "" [prefix; Bytes.to_string entry.k] in
-         Util.ensure_valid_key path ;
-         (path, entries @ [{layer= 0; key= path; value= entry.v; right= None}]) )
-       ("", [])
-  |> snd
+         Util.ensure_valid_key path ; path :: keys )
+       []
+  |> List.rev
 
 module Make (Store : Storage.Writable_blockstore) = struct
   type bs = Store.t
@@ -106,7 +117,7 @@ module Make (Store : Storage.Writable_blockstore) = struct
   let create blockstore root = {blockstore; root}
 
   (* decodes a node retrieved from the blockstore *)
-  let decode_block b : node_raw =
+  let decode_block_raw b : node_raw =
     match Dag_cbor.decode b with
     | `Map node ->
         if not (StringMap.mem "e" node) then
@@ -157,21 +168,64 @@ module Make (Store : Storage.Writable_blockstore) = struct
     | _ ->
         raise (Invalid_argument "invalid block")
 
-  (* retrieves & decodes a node by cid *)
-  let retrieve_node t cid : node_raw option Lwt.t =
+  let retrieve_node_raw t cid : node_raw option Lwt.t =
     match%lwt Store.get_bytes t.blockstore cid with
     | Some bytes ->
-        Lwt.return_some (decode_block bytes)
+        bytes |> decode_block_raw |> Lwt.return_some
     | None ->
         Lwt.return_none
 
+  (* retrieves & decodes a node by cid *)
+  let rec retrieve_node t cid : node option Lwt.t =
+    match%lwt retrieve_node_raw t cid with
+    | Some raw ->
+        hydrate_node t raw |> Lwt.map Option.some
+    | None ->
+        Lwt.return_none
+
+  and retrieve_node_lazy t cid = lazy (retrieve_node t cid)
+
+  (* hydrates a raw node *)
+  and hydrate_node t node_raw : node Lwt.t =
+    let left =
+      match node_raw.l with
+      | Some l ->
+          retrieve_node_lazy t l
+      | None ->
+          lazy Lwt.return_none
+    in
+    let%lwt layer = get_node_height t node_raw in
+    let entries =
+      List.fold_left
+        (fun entries entry ->
+          let prefix =
+            match entries with
+            | [] ->
+                ""
+            | prev :: _ ->
+                String.sub prev.key 0 entry.p
+          in
+          let path = String.concat "" [prefix; Bytes.to_string entry.k] in
+          Util.ensure_valid_key path ;
+          let right =
+            match entry.t with
+            | Some r ->
+                retrieve_node_lazy t r
+            | None ->
+                lazy Lwt.return_none
+          in
+          {layer; key= path; value= entry.v; right} :: entries )
+        [] node_raw.e
+    in
+    Lwt.return {layer; left; entries}
+
   (* returns the layer of a node *)
-  let rec get_node_height t node : int Lwt.t =
+  and get_node_height t node : int Lwt.t =
     match (node.l, node.e) with
     | None, [] ->
         Lwt.return 0
     | Some left, [] -> (
-        match%lwt retrieve_node t left with
+        match%lwt retrieve_node_raw t left with
         | Some node ->
             let%lwt height = get_node_height t node in
             Lwt.return (height + 1)
@@ -188,23 +242,10 @@ module Make (Store : Storage.Writable_blockstore) = struct
   let traverse t fn : unit Lwt.t =
     let rec traverse node =
       let%lwt () =
-        match node.l with
-        | Some cid -> (
-            match%lwt retrieve_node t cid with
-            | Some node ->
-                traverse node
-            | None ->
-                Lwt.return_unit )
-        | None ->
-            Lwt.return_unit
+        let*? left = node.left in
+        match left with Some l -> traverse l | None -> Lwt.return_unit
       in
-      ignore
-        (List.fold_left
-           (fun prev_path entry ->
-             let prefix = String.sub prev_path 0 entry.p in
-             let path = String.concat "" [prefix; Bytes.to_string entry.k] in
-             fn path entry.v ; path )
-           "" node.e ) ;
+      List.iter (fun entry -> fn entry.key entry.value) node.entries ;
       Lwt.return_unit
     in
     match%lwt retrieve_node t t.root with
@@ -221,71 +262,77 @@ module Make (Store : Storage.Writable_blockstore) = struct
     in
     Lwt.return map
 
-  (* produces a hydrated mst from a map of key -> cid *)
-  let hydrate_from_map t map : Cid.t Lwt.t =
+  (* produces a cid and cbor-encoded bytes for this mst *)
+  let serialize t map : (Cid.t * bytes) Lwt.t =
     let keys =
       map |> StringMap.bindings |> List.map fst |> List.sort String.compare
     in
     let entry_for_key key =
       let value = StringMap.find key map in
       let height = Util.leading_zeros_on_hash key in
-      {layer= height; key; value; right= None}
+      {layer= height; key; value; right= lazy Lwt.return_none}
     in
     let root =
       { layer= keys |> List.hd |> Util.leading_zeros_on_hash
       ; entries= []
-      ; left= None }
+      ; left= lazy Lwt.return_none }
     in
     List.iter
       (fun key -> ignore (insert_entry root (entry_for_key key)))
       (List.tl keys) ;
-    let rec finalize node : Cid.t Lwt.t =
-      let left =
-        match node.left with
+    let rec finalize node : (Cid.t * bytes) Lwt.t =
+      let%lwt left =
+        node.left
+        >>? function
         | Some l ->
-            Some (Lwt_main.run (finalize l))
+            let%lwt cid, _ = finalize l in
+            Lwt.return_some cid
         | None ->
-            None
+            Lwt.return_none
       in
       let last_key = ref "" in
-      let mst_entries =
-        List.map
+      let%lwt mst_entries =
+        Lwt_list.map_s
           (fun entry ->
-            let right =
-              match entry.right with
+            let%lwt right =
+              entry.right
+              >>? function
               | Some r ->
-                  Some (Lwt_main.run (finalize r))
+                  let%lwt cid, _ = finalize r in
+                  Lwt.return (Some cid)
               | None ->
-                  None
+                  Lwt.return None
             in
             let prefix_len = Util.shared_prefix_length !last_key entry.key in
             last_key := entry.key ;
-            { k=
-                Bytes.of_string
-                  (String.sub entry.key prefix_len
-                     (String.length entry.key - prefix_len) )
-            ; p= prefix_len
-            ; v= entry.value
-            ; t= right } )
+            Lwt.return
+              { k=
+                  Bytes.of_string
+                    (String.sub entry.key prefix_len
+                       (String.length entry.key - prefix_len) )
+              ; p= prefix_len
+              ; v= entry.value
+              ; t= right } )
           node.entries
       in
-      let mst_node = {l= left; e= mst_entries} in
-      let encoded = Dag_cbor.encode (encode_node_raw mst_node) in
+      let encoded =
+        Dag_cbor.encode (encode_node_raw {l= left; e= mst_entries})
+      in
       let cid = Cid.create Dcbor encoded in
       let%lwt () = Store.put_block t.blockstore cid encoded in
-      Lwt.return cid
+      Lwt.return (cid, encoded)
     in
     finalize root
 
   (* returns cids and blocks that form the path from a given node to a given entry *)
   let rec path_to_entry t node key : (Cid.t * bytes) list Lwt.t =
-    let%lwt root_bytes = Store.get_bytes t node in
+    let%lwt root_bytes = Store.get_bytes t.blockstore node in
     let%lwt root =
       match root_bytes with
       | None ->
           Lwt.return_none
       | Some bytes ->
-          Lwt.return_some (decode_block bytes)
+          Lwt.return_some (decode_block_raw bytes)
     in
     let path_tail = [(node, Option.get root_bytes)] in
     (* if there is a left child, try to find a path through the left subtree *)
@@ -310,11 +357,11 @@ module Make (Store : Storage.Writable_blockstore) = struct
         Lwt.return path
     | None -> (
         (* if a left subtree path couldn't be found, find the entry whose right subtree this key would belong to *)
-        let root' = Option.get root in
-        let entries_keys = hydrate_entries_keys_only root' in
-        let entries_len = List.length root'.e in
+        let entries = (Option.get root).e in
+        let entries_keys = entries_to_keys entries in
+        let entries_len = List.length entries in
         let entry_index =
-          match List.find_index (fun e -> e.key >= key) entries_keys with
+          match List.find_index (fun e -> e >= key) entries_keys with
           | Some index ->
               index
           | None ->
@@ -325,11 +372,11 @@ module Make (Store : Storage.Writable_blockstore) = struct
         | _
         (* because entries[entry_index] might turn out to be the entry we're looking for *)
           when entry_index < entries_len
-               && (List.nth entries_keys entry_index).key = key ->
+               && List.nth entries_keys entry_index = key ->
             Lwt.return path_tail
         | _ -> (
           (* otherwise, we continue down the right subtree of the entry before entry_index *)
-          match Util.last root'.e with
+          match Util.last entries with
           | Some last when last.t != None ->
               let%lwt path_through_right =
                 path_to_entry t (Option.get last.t) key
@@ -339,18 +386,16 @@ module Make (Store : Storage.Writable_blockstore) = struct
               Lwt.return path_tail ) )
 
   (* returns all mst entries in order for a car stream *)
-  let to_car_stream t : (Cid.t * bytes) Seq.t =
+  let to_blocks_seq t : (Cid.t * bytes) Lwt_seq.t =
     let module M = struct
       type stage =
+        (* currently walking nodes *)
         | Nodes of
-            (* currently walking nodes *)
-            
             { next: Cid.t list (* next cids to fetch *)
             ; fetched: (Cid.t * bytes) list (* fetched cids and their bytes *)
             ; leaves: Cid.Set.t (* seen leaf cids *) }
-        | Leaves of
-            (* done walking nodes, streaming accumulated leaves *)
-            (Cid.t * bytes) list
+        (* done walking nodes, streaming accumulated leaves *)
+        | Leaves of (Cid.t * bytes) list
         | Done
     end in
     let open M in
@@ -359,30 +404,28 @@ module Make (Store : Storage.Writable_blockstore) = struct
     in
     let rec step = function
       | Done ->
-          None
+          Lwt.return_none
       (* node has been fetched, can now be yielded *)
       | Nodes ({fetched= (cid, bytes) :: rest; _} as s) ->
-          Some ((cid, bytes), Nodes {s with fetched= rest})
+          Lwt.return_some ((cid, bytes), Nodes {s with fetched= rest})
       (* need to fetch next nodes *)
       | Nodes {next; fetched= []; leaves} ->
           if List.is_empty next then (
             (* finished traversing nodes, time to switch to leaves *)
             let leaves_list = Cid.Set.to_list leaves in
-            let leaves_bm =
-              Lwt_main.run (Store.get_blocks t.blockstore leaves_list)
-            in
+            let%lwt leaves_bm = Store.get_blocks t.blockstore leaves_list in
             if leaves_bm.missing <> [] then failwith "missing mst leaf blocks" ;
             let leaves_nodes = Storage.Block_map.entries leaves_bm.blocks in
             match leaves_nodes with
             | [] ->
                 (* with Done, we don't care about the first pair element *)
-                Some (Obj.magic (), Done)
+                Lwt.return_some (Obj.magic (), Done)
             | _ ->
                 (* it's leafin time *)
                 step (Leaves leaves_nodes) )
           else
             (* go ahead and fetch the next nodes *)
-            let bm = Lwt_main.run (Store.get_blocks t.blockstore next) in
+            let%lwt bm = Store.get_blocks t.blockstore next in
             if bm.missing <> [] then failwith "missing mst nodes" ;
             let fetched, next', leaves' =
               List.fold_left
@@ -391,12 +434,13 @@ module Make (Store : Storage.Writable_blockstore) = struct
                     (* we should be safe to do this since we just got the cids from the blockmap *)
                     Storage.Block_map.get cid bm.blocks |> Option.get
                   in
-                  let node = decode_block bytes in
+                  let node = decode_block_raw bytes in
                   let nxt' =
                     List.fold_left
                       (* node.entries.map(e => e.right) *)
                       (fun n e -> match e.t with Some c -> c :: n | None -> n )
                       (* start with [node.left, ...nxt] if node has a left subtree *)
+                      (* next' looks like [..., n_2.r_2, n_2.l, n_1.r_n, ..., n_1.r_1, n_1.l]) *)
                       ( match node.l with
                       | Some l ->
                           l :: nxt
@@ -420,10 +464,10 @@ module Make (Store : Storage.Writable_blockstore) = struct
       (* if we're onto yielding leaves, do that *)
       | Leaves ((cid, bytes) :: rest) ->
           let next = if rest = [] then Done else Leaves rest in
-          Some ((cid, bytes), next)
+          Lwt.return_some ((cid, bytes), next)
       (* once we're out of leaves, we're done *)
       | Leaves [] ->
-          Some (Obj.magic (), Done)
+          Lwt.return_some (Obj.magic (), Done)
     in
-    Seq.unfold step init_state
+    Lwt_seq.unfold_lwt step init_state
 end
