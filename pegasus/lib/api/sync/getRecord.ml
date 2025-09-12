3a9b578d57d127c12b8a956a3c4e6e9f21050dfe
futurGH <me@futuristick.ca>
2025-09-11 22:58:36 -0400

    Return inclusion/exclusion proof from sync.getRecord

--- pegasus/lib/api/sync/getRecord.ml
+++ pegasus/lib/api/sync/getRecord.ml
@@ -1,6 +1,6 @@
-type query = {did: string; collection: string; rkey: string} [@@deriving yojson]
+module Mst = Mist.Mst.Make (User_store)
 
-type response = Mist.Lex.repo_record [@@deriving yojson]
+type query = {did: string; collection: string; rkey: string} [@@deriving yojson]
 
 let handler =
   Xrpc.handler (fun ctx ->
@@ -8,9 +8,26 @@ let handler =
         Xrpc.parse_query ctx.req query_of_yojson
       in
       let path = collection ^ "/" ^ rkey in
-      let%lwt {db; _} = Repository.load did ~write:false ~ds:ctx.db in
-      match%lwt User_store.get_record_by_path db path with
-      | Some {value; _} ->
-          Dream.json @@ Yojson.Safe.to_string @@ response_to_yojson value
-      | None ->
-          Errors.invalid_request ~name:"RecordNotFound" "record not found" )
+      let%lwt {db; commit; _} = Repository.load did ~write:false ~ds:ctx.db in
+      let commit_cid, commit_signed = Option.get commit in
+      let commit_block =
+        commit_signed |> User_store.Types.signed_commit_to_yojson
+        |> Dag_cbor.encode_yojson
+      in
+      let mst_root = commit_signed.data in
+      let%lwt blocks =
+        Mst.proof_for_key {blockstore= db; root= mst_root} mst_root path
+      in
+      let blocks_stream =
+        Repository.BlockMap.entries blocks |> Lwt_seq.of_list
+      in
+      let car_stream =
+        Lwt_seq.cons (commit_cid, commit_block) blocks_stream
+        |> Car.blocks_to_stream commit_cid
+      in
+      Dream.stream
+        ~headers:[("Content-Type", "application/vnd.ipld.car")]
+        (fun res_stream ->
+          Lwt_seq.iter_s
+            (fun chunk -> Dream.write res_stream (Bytes.to_string chunk))
+            car_stream ) )
