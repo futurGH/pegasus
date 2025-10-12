b83018a40c905ff20f8f5bd4e610655783a21083
futurGH <me@futuristick.ca>
2025-10-12 00:54:00 -0400

    ES256K

--- pegasus/lib/api/well_known.ml
+++ pegasus/lib/api/well_known.ml
@@ -40,6 +40,7 @@ let oauth_authorization_server =
            ; ( "scopes_supported"
              , `List
                  [ `String "atproto"
+                 ; `String "transition:email"
                  ; `String "transition:generic"
                  ; `String "transition:chat.bsky" ] )
            ; ("subject_types_supported", `List [`String "public"])
@@ -51,14 +52,17 @@ let oauth_authorization_server =
            ; ( "code_challenge_methods_supported"
              , `List [`String "S256"; `String "plain"] )
            ; ("ui_locales_supported", `List [`String "en-US"])
-           ; ("display_values_supported", `List [`String "page"])
+           ; ( "display_values_supported"
+             , `List [`String "page"; `String "popup"; `String "touch"] )
            ; ("authorization_response_iss_parameter_supported", `Bool true)
            ; ( "request_object_signing_alg_values_supported"
-             , `List [`String "none"] )
+             , `List [`String "ES256"; `String "ES256K"] )
            ; ("request_object_encryption_alg_values_supported", `List [])
            ; ("request_object_encryption_enc_values_supported", `List [])
-           ; ("token_endpoint_auth_methods_supported", `List [`String "none"])
+           ; ( "token_endpoint_auth_methods_supported"
+             , `List [`String "none"; `String "private_key_jwt"] )
            ; ( "token_endpoint_auth_signing_alg_values_supported"
-             , `List [`String "ES256"] )
-           ; ("dpop_signing_alg_values_supported", `List [`String "ES256"])
+             , `List [`String "ES256"; `String "ES256K"] )
+           ; ( "dpop_signing_alg_values_supported"
+             , `List [`String "ES256"; `String "ES256K"] )
            ; ("client_id_metadata_document_supported", `Bool true) ] )
