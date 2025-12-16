b0056cb45be865f0a0defbf835f574b351aa50bb
futurGH <me@futuristick.ca>
2025-12-15 23:51:01 -0500

    Attempt at fixing deadlock when subscribeRepos consumer disconnects

--- pegasus/lib/api/sync/subscribeRepos.ml
+++ pegasus/lib/api/sync/subscribeRepos.ml
@@ -8,10 +8,33 @@ let handler =
             | None ->
                 0
           in
+          let closed = ref false in
           let send (bytes : bytes) =
-            Dream.send ~text_or_binary:`Binary ws (Bytes.unsafe_to_string bytes)
+            if !closed then Lwt.fail Exit
+            else
+              Lwt.catch
+                (fun () ->
+                  Lwt_unix.with_timeout 30.0 (fun () ->
+                      Dream.send ~text_or_binary:`Binary ws
+                        (Bytes.unsafe_to_string bytes) ) )
+                (fun _ ->
+                  closed := true ;
+                  Lwt.fail Exit )
+          in
+          let stream () =
+            Sequencer.Live.stream_with_backfill ~conn:db ~cursor ~send
+          in
+          let wait_for_close () =
+            let rec loop () =
+              match%lwt Dream.receive ws with
+              | Some _ ->
+                  loop ()
+              | None ->
+                  closed := true ;
+                  Lwt.fail Exit
+            in
+            loop ()
           in
           Lwt.catch
-            (fun () ->
-              Sequencer.Live.stream_with_backfill ~conn:db ~cursor ~send )
+            (fun () -> Lwt.pick [stream (); wait_for_close ()])
             (fun _exn -> Lwt.return_unit) ) )
